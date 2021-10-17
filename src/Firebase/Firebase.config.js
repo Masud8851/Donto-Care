844d092af606b2b5b794874129c48c2ffc2581d7
Masud8851 <masud1971.cse@gmail.com>
2021-10-17 23:04:52 +0600

    Add

---
+++ src/Firebase/Firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+	apiKey: "AIzaSyCCAl--W8p1HDcHn8rXbDmZ8cRtj9gOC4Y",
+	authDomain: "donto-care.firebaseapp.com",
+	projectId: "donto-care",
+	storageBucket: "donto-care.appspot.com",
+	messagingSenderId: "129110979192",
+	appId: "1:129110979192:web:6320bbf1b2938b4fc3f38d",
+};
+
+export default firebaseConfig;
