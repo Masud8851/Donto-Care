17dae497de5b07a9979117c93bf665a38e7af624
Masud8851 <masud1971.cse@gmail.com>
2021-10-18 14:00:18 +0600

    ADD

--- src/Pages/Services/Services.js
+++ src/Pages/Services/Services.js
@@ -1,32 +1,47 @@
 import React from "react";
 import Navbar from "../NavBar/Navbar";
 import "./Services.css";
+import first from "../../image/About/about-4.png";
 
 const Services = () => {
 	return (
 		<div>
-			<Navbar></Navbar>
-			<div>
-				<div className="container">
-					<div className="row">
-						<div className="col-md-6 p-3">
-							<h1>Service Details</h1>
-							<p>
-								Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam,
-								hic?
-							</p>
-						</div>
-						<div className="col-md-6 p-3">
-							<div className="col-12 col-md-6 col-lg-6">
-								<div className="page-banner-img">
-									<div className="banner-animate">
-										<img src="" alt="" className="a1" />
-										<img src="" className="bannerimg a3" alt="" />
-										<img src="" className="bannerimg a4" alt="" />
-										<img src="" className="bannerimg a5" alt="" />
-									</div>
+			<div className="services-head">
+				<Navbar></Navbar>
+				<div>
+					<div className="container">
+						<div className="row align-items-center justify-content-center p-3">
+							<div className="col-md-6 p-3">
+								<h1>Service Details</h1>
+								<p>
+									Lorem ipsum dolor sit amet consectetur adipisicing elit.
+									Ipsam, hic?
+								</p>
+							</div>
+							<div className="col-md-6 p-3">
+								<div>
+									<img src={first} id="teeth" alt="" className="p-5" />
 								</div>
 							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<h1>Services</h1>
+			<div class="row row-cols-1 row-cols-md-3 g-4">
+				<div class="col">
+					<div class="card h-100">
+						<img src="..." class="card-img-top" alt="..." />
+						<div class="card-body">
+							<h5 class="card-title">Card title</h5>
+							<p class="card-text">
+								This is a wider card with supporting text below as a natural
+								lead-in to additional content. This content is a little bit
+								longer.
+							</p>
+						</div>
+						<div class="card-footer">
+							<small class="text-muted">Last updated 3 mins ago</small>
 						</div>
 					</div>
 				</div>
